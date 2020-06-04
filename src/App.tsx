99e4ecc2ed98c4bc0682cffd884ae62e028b0f46
Lazeratrax <lazeratrax@gmail.com>
2020-06-04 23:43:59 +0300

    auth:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ApolloProvider } from '@apollo/client'
+import { Redirect, Route, Router, Switch, Link } from 'react-router-dom'
+import { NAV } from './nav'
+import history from './utils/history'
+import NotFound from './views/404'
+import { client } from './graphql'
+import Login from './auth/Login'
+import Logout from './auth/Logout'
+import Registration from './auth/Registration'
+import { AuthContext } from './auth/AuthContext'
+import AuthRoute, { useCurrentUser } from './auth/AuthRoute'
+import Page from './views/Page'
+import { Layout, Menu } from 'antd'
+import {
+  IdcardOutlined as IconIdCard,
+  FileTextOutlined as IconFile,
+  LogoutOutlined as IconLogout
+} from '@ant-design/icons'
+import PostsPage from './views/posts/Posts'
+import PostPage from './views/post/Post'
 
-function App() {
+const AppSidebar: React.FC = () => {
+  const user = useCurrentUser()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Layout.Sider collapsible>
+        <Menu theme="dark" mode="inline">
+          <Menu.Item key={NAV.Posts}>
+            <IconFile/>
+            <span>Главная</span>
+            <Link to={NAV.Posts}/>
+          </Menu.Item>
+          <Menu.Item key={NAV.Profile}>
+            <IconIdCard/>
+            <span>Профиль</span>
+            <Link to={NAV.Profile}/>
+          </Menu.Item>
+          {user && <Menu.Item key={NAV.Logout}>
+            <IconLogout/>
+            <span>Выйти</span>
+            <Link to={NAV.Logout}/>
+          </Menu.Item>}
+        </Menu>
+      </Layout.Sider>
+  )
 }
 
-export default App;
+const AppLayout = () => (
+    <Layout style={{ minHeight: '100vh', backgroundColor: '#FFF' }}>
+      <AppSidebar/>
+      <Layout.Content>
+        <Switch>
+          <Route exact path={NAV.App}>
+            <Redirect to={NAV.Posts}/>
+          </Route>
+          <Route path={NAV.Posts} component={PostsPage}/>
+          <Route path={NAV.Post} component={PostPage}/>
+          <Route path={NAV.Profile} component={Page}/>
+        </Switch>
+      </Layout.Content>
+    </Layout>
+)
+
+const App: React.FC = () => (
+    <ApolloProvider client={client}>
+      <Router history={history}>
+        <AuthContext>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to={NAV.Posts}/>
+            </Route>
+            <AuthRoute path={NAV.App} component={AppLayout}/>
+            <Route path={NAV.Login} component={Login}/>
+            <Route path={NAV.Logout} component={Logout}/>
+            <Route path={NAV.Registration} component={Registration}/>
+            <Route component={NotFound}/>
+          </Switch>
+        </AuthContext>
+      </Router>
+    </ApolloProvider>
+)
+
+export default App
+
